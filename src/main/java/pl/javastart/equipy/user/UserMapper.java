ed554c621d7de145ec08300a033b0a0b99b4311a
DamianJarzebowski <damian.r.hajduk@o2.pl>
2022-03-29 02:12:10 +0200

    WIP

--- src/main/java/pl/javastart/equipy/user/UserMapper.java
+++ src/main/java/pl/javastart/equipy/user/UserMapper.java
@@ -4,6 +4,11 @@ public class UserMapper {
 
     static  UserDto toDto(User user) {
         UserDto userDto = new UserDto();
+
         userDto.setId(user.getId());
+        userDto.setFirstName(user.getFirstName());
+        userDto.setLastName(user.getLastName());
+        userDto.setPesel(user.getPesel());
+        return userDto;
     }
 }
