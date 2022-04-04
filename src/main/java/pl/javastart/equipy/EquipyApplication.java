e0984da0ae1896c75af85bd38295372f0a01ca76
DamianJarzebowski <damian.r.hajduk@o2.pl>
2022-04-04 20:58:02 +0200

    WIP

--- src/main/java/pl/javastart/equipy/EquipyApplication.java
+++ src/main/java/pl/javastart/equipy/EquipyApplication.java
@@ -6,7 +6,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class EquipyApplication {
 
+    // v 1.2.0
+
     public static void main(String[] args) {
         SpringApplication.run(EquipyApplication.class, args);
     }
+
+
 }
